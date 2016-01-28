2b39f08296530ca6be8da9d21a14ae7c7a23e764
xiaofeng <xiaofeng@DESKTOP-F4Q31PN>
2016-01-28 17:01:31 +0800

    test

---
+++ src/test/java/service/Test.java
@@ -0,0 +1,13 @@
+package service;
+
+
+import core.BaseTest;
+
+
+public class Test extends BaseTest {
+	public static void main(String[] args) {
+		String str= "abc";
+		 String a = new String();
+		 System.out.println();
+	}
+}
